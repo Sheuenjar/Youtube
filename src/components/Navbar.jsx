e77161637ba52e88be9c8d7344cf147194e9ae81
Sheuen Jaramillo <sheuenjaramillo@gmail.com>
2022-12-26 01:58:13 -0300

    SearchBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 
 
+import { Search } from '@mui/icons-material';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
 
 
 
@@ -18,6 +20,7 @@ const Navbar = () => (
     <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
       <img src={logo} alt='logo' height={45} />
     </Link>
+    <SearchBar />
   </Stack>
 )
 
