8e88e8e2c4cd72215794da7539aeeb7793486db3
Sheuen Jaramillo <sheuenjaramillo@gmail.com>
2022-12-27 23:10:34 -0300

    VideoCard functionality

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,10 +2,12 @@
 
 
 import { Link } from 'react-router-dom';
-import { Typography, Card, CardContent, CardMedia } from '@mui/icons-material';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
+
+
 
 
 
@@ -14,10 +16,17 @@ const VideoCard = ({ video: {
 } }) => {
 
 
+
   return (
     <Card>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-        <CardMedia image={snippet?.thumbnails?.high?.url} />
+
+        <CardMedia 
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}  
+        />
+
       </Link>
     </Card>
   )
