cc3074d6ec9c28b9ebc56a27d2cacddd500e1d0a
Sheuen Jaramillo <sheuenjaramillo@gmail.com>
2022-12-31 00:41:43 -0300

    logo debug

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -25,21 +25,23 @@ const ChannelCard = ({ channelDetail }) => (
     >
 
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
-        <CardContent sx={{ display:'flex', flexDirection:'column', justifyContent: 'center', textAlign:'center', color:'#fff' }}>
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
 
           <CardMedia 
-            image={channelDetail?.snipper?.thumbnails?.high?.url || demoProfilePicture}
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
             alt={channelDetail?.snippet?.title}
-            sx={{ borderRadius:'50%', height:'180px', width:'180px', mb:2, border:'1px solid #e3e3e3' }}
+            sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
           />
 
           <Typography variant='h6'>
             {channelDetail?.snippet?.title}
             <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
           </Typography>
+
           {channelDetail?.statistics?.subscriberCount && (
             <Typography>
-              {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+              {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} 
+              Subscribers
             </Typography>
           )}
         </CardContent>
