d745a6b64c8bb61087b3341effd45812a5ccc182
sheuen <sheuenjaramillo@gmail.com>
2023-03-24 17:08:28 -0300

    related videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -9,12 +9,13 @@ import { VideoCard, ChannelCard } from './';
 
 
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
 
+  if(!videos?.length) return 'Loading...';
 
   return (
     <Stack
-        direction='row'
+        direction={ direction || 'row' }
         flexWrap='wrap'
         justifyContent='start'
         gap={2}
