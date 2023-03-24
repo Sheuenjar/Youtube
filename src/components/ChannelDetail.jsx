eb072692b37dad978118a0b1925535642a476140
sheuen <sheuenjaramillo@gmail.com>
2023-03-24 00:25:34 -0300

    submiting youtubeclone with artistDetails and showing videos capacity

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,51 @@
+
+
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+
+
+const ChannelDetail = () => {
+
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  const { id } = useParams();
+
+  console.log(channelDetail, videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
+  }, [id])
+
+  return (
+    <Box minHeight='95vh'>
+      <Box>
+        <div 
+          style={{
+            background: 'linear-gradient(90deg, rgba(36,0,0,1) 0%, rgba(21,50,2,1) 24%, rgba(121,9,9,1) 47%, rgba(18,8,166,1) 71%, rgba(10,66,14,1) 100%)',
+            zIndex: 10,
+            height: '300px'
+          }}
+        />
+          <ChannelCard channelDetail={channelDetail} marginTop='-110px'/>
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{ mr: { sm: '100px' }}} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  )
+}
+
+
+
+export default ChannelDetail
