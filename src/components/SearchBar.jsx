2ca33afc49a9132647a6fa56973e2f2a436c1a1d
sheuen <sheuenjaramillo@gmail.com>
2023-03-25 21:44:10 -0300

    searchTerm should still stay

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -21,7 +21,7 @@ const SearchBar = () => {
         if(searchTerm) {
             navigate(`/search/${searchTerm}`);
 
-            setSearchTerm('');
+            /* setSearchTerm(''); */
         }
     }
 
